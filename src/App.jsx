2a4f5c99626aa8b46a1ee19c6b6a037af3d22b11
alaminwbsoft <wbsoft.alamin@gmail.com>
2025-03-07 00:33:21 +0600

    project setup and navbar add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
